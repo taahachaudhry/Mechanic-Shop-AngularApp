bf5ca90ca1dac22dc4b6232f09ab60eba48aa08f
taahachaudhry <t44h413@gmail.com>
2014-08-13 10:53:55 -0500

    Can add cars to table

--- AngularIntro/Scripts/_script.js
+++ AngularIntro/Scripts/_script.js
@@ -1,1 +1,16 @@
-﻿
+﻿var MechanicShop = angular.module('MechanicShop', []);
+
+MechanicShop.controller('MechanicShopController', function ($scope) {
+    $scope.cars = [];
+    $scope.addCar = function () {
+        var car = {};
+        car.make = $scope.make;
+        car.model = $scope.model;
+        car.year = $scope.year;
+        car.fixed = false;
+        $scope.cars.push(car);
+        $scope.make = ''
+        $scope.model = ''
+        $scope.year = ''
+    }
+});
