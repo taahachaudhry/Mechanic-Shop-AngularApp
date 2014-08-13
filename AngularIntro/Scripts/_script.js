19ee354f6bc216015d021c2503d83f45faff4b92
taahachaudhry <t44h413@gmail.com>
2014-08-13 12:02:50 -0500

    Added DetailsController and Factory

--- AngularIntro/Scripts/_script.js
+++ AngularIntro/Scripts/_script.js
@@ -1,20 +1,26 @@
 ﻿var MechanicShop = angular.module('MechanicShop', []);
 
-MechanicShop.controller('MechanicShopController', function ($scope) {
-    $scope.cars = [{ "make": "BMW", "model": "330Ci", "year": "2009", "fixed": false }, { "make": "Honda", "model": "Accord", "year": "2013", "fixed": false }, { "make": "Tesla", "model": "ModelX", "year": "2013", "fixed": false }];
+MechanicShop.factory('CarFactory', function () {
+    return [{ "make": "BMW", "model": "330Ci", "year": "2009", "fixed": false }, { "make": "Honda", "model": "Accord", "year": "2013", "fixed": false }, { "make": "Tesla", "model": "ModelX", "year": "2013", "fixed": false }];
+})
+MechanicShop.controller('TableController', function (CarFactory, $scope) {
+    $scope.cars = CarFactory;
     $scope.addCar = function () {
-        var car = {};
+        var car = {};    
         car.make = $scope.make;
         car.model = $scope.model;
         car.year = $scope.year;
         car.fixed = false;
-        $scope.cars.push(car);
-        $scope.make = ''
-        $scope.model = ''
-        $scope.year = ''
+        CarFactory.push(car);
+        $scope.make = '';
+        $scope.model = '';
+        $scope.year = '';
     }
     $scope.removeCar = function (car) {
         var index = $scope.cars.indexOf(car);
         $scope.cars.splice(index, 1);
     }
 });
+MechanicShop.controller('DetailsController', function ($scope) {
+
+})
